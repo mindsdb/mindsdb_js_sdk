0602a816963589e302bac7e4585f82157f13ef4b
sebastian tobon <setohe.09@gmail.com>
2019-12-23 16:59:16 -0500

    [aut] - use the same conext fill for connection

--- src/utils.js
+++ src/utils.js
@@ -6,19 +6,11 @@ export const setQueryParams = (paramsObj, url) => {
       const key = encodeURIComponent(item.key); 
       const value = encodeURIComponent(item.value);
   
-      let s = '';
       const kvp = key + '=' + value;
-      const r = new RegExp('(&|\\?)'+key+'=[^\&]*');
-  
-      s = s.replace(r, '$1' + kvp);
-  
-      if(!RegExp.$1) {
-        s += (i != 0 ? '&' : '?') + kvp;
-      }
-       params = params.concat(s);
+      params = params.concat(kvp);
     });
     
-    return params;
+    return url + '?' + params;
   }
 
   return url;
